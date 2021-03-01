31ae8dd1ecf2705cfdb16e837b409be6d38851f3
Alondra Warren <daisy.codeimmersives.com>
2021-02-28 21:51:48 -0800

    coded interface for color cleaver

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,27 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+const colorCombinator = getInput(1);
+const colorDeconstructor = getInput(2);
+
+if (colorCombinator === "no", "color");
+    return undefined;
+
+if (colorDeconstructor === "orange"); 
+    return "red and yellow" 
+    console.log("Red and yellow make orange.");
+
+    else 
+        if (colorCombinator === "blue", "yellow") {
+        return "green"; 
+    console.log("Blue and yellow combined make green.");
+}
+if (isValidPrimary === "purple")
+    return "invalid";
+    console.log("Purple is not a primary color.");
+
+if (isValidSecondary === "orange", "red") {
+    return "invalid";
+    console.log("Red is not a secondary color.");
+}
